183d6e9ddb976b2b506553174fee0ea9d3f63a7d
Nguyen Giap <hgiap1804@gmail.com>
2026-02-17 20:55:29 +0700

    add docker compose

--- src/main/java/com/example/scan3d/dtos/request/auth/RegisterRequest.java
+++ src/main/java/com/example/scan3d/dtos/request/auth/RegisterRequest.java
@@ -2,16 +2,27 @@ package com.example.scan3d.dtos.request.auth;
 
 import com.example.scan3d.dtos.request.BaseDTO;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RegisterRequest extends BaseDTO {
     @JsonProperty("first_name")
+    @NotNull(message = "First name must be not null")
     private String firstName;
+
     @JsonProperty("last_name")
+    @NotNull(message = "Last name must be not null")
     private String lastName;
+
     @JsonProperty("email")
+    @NotNull(message = "Email must be not null")
+    @Email(message = "Invalid email format")
     private String email;
+
     @JsonProperty("password")
+    @NotNull(message = "Password must be not null")
     private String password;
 }
